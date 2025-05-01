9ec203498e3d0ae073de26136ab15512ef6e3d40
Evgenia Bullis <bulish2015@yandex.ru>
2025-05-01 11:30:22 +0300

    add common function.

---
+++ src/main/java/com/bulish/melnikov/converter/functions/ConverterFileFunction.java
@@ -0,0 +1,27 @@
+package com.bulish.melnikov.converter.functions;
+
+import com.bulish.melnikov.converter.model.ConvertRequestMsgDTO;
+import com.bulish.melnikov.converter.service.ConverterRequestQueueManagerService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.function.Consumer;
+
+@Slf4j
+@RequiredArgsConstructor
+@Configuration
+public class ConverterFileFunction {
+
+    private final ConverterRequestQueueManagerService queueService;
+
+    @Bean
+    public Consumer<ConvertRequestMsgDTO> convert() {
+        return convertRequest -> {
+            log.info("Request to convert arrived : from {} to {}",
+                    convertRequest.getFormatFrom(), convertRequest.getFormatTo());
+            queueService.addRequestToQueue(convertRequest);
+        };
+    }
+}
